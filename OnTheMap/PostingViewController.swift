23045c2796b031919bcc5bb2ad08538c1d79209b
Kelly Innes <kinnes@gmail.com>
2015-07-01 21:57:08 -0400

    setup storyboard and basic user flow

---
+++ OnTheMap/PostingViewController.swift
@@ -0,0 +1,38 @@
+//
+//  PostingViewController.swift
+//  OnTheMap
+//
+//  Created by Kelly Innes on 7/1/15.
+//  Copyright (c) 2015 Kelly Innes. All rights reserved.
+//
+
+import UIKit
+
+class PostingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func dismissPostingViewController(sender: UIBarButtonItem) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
