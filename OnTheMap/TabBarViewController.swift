b004be7f4167febf35ad9fc86d002d2792d9c9d7
Kelly Innes <kinnes@gmail.com>
2015-07-06 23:58:08 -0400

    finish shared navigationbar buttons

--- OnTheMap/TabBarViewController.swift
+++ OnTheMap/TabBarViewController.swift
@@ -12,15 +12,15 @@ class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let pinImg: UIImage = UIImage(named: "pinicon")!
         let refreshButton = UIBarButtonItem(barButtonSystemItem: .Refresh, target: self, action: "refresh")
         let logoutButton = UIBarButtonItem(title: "Logout", style: .Plain, target: self, action: "logout")
-        self.navigationItem.rightBarButtonItem = refreshButton
+        let pinButton = UIBarButtonItem(image: pinImg, style: UIBarButtonItemStyle.Plain, target: self, action: "pin")
+        let rightButtons = [refreshButton, pinButton]
+        self.navigationItem.rightBarButtonItems = rightButtons
         self.navigationItem.leftBarButtonItem = logoutButton
-        
         self.title = "On The Map"
-        println("hello world")
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,6 +34,10 @@ class TabBarViewController: UITabBarController {
     
     func refresh() {
         println("refresh")
+    }
+    
+    func pin() {
+        println("pin")
     }
     
 
