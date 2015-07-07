83dd0089017a81d3fabbd9f00e303176ebce1c03
Kelly Innes <kinnes@gmail.com>
2015-07-06 23:52:26 -0400

    add navbar and progammatically add buttons

---
+++ OnTheMap/RootNavViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RootNavViewController.swift
+//  OnTheMap
+//
+//  Created by Kelly Innes on 7/6/15.
+//  Copyright (c) 2015 Kelly Innes. All rights reserved.
+//
+
+import UIKit
+
+class RootNavViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func helloWorld() {
+        println("Hello World")
+    }
+
+}
--- OnTheMap/TabBarViewController.swift
+++ OnTheMap/TabBarViewController.swift
@@ -8,12 +8,19 @@
 
 import UIKit
 
-class TabBarViewController: UIViewController {
+class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let refreshButton = UIBarButtonItem(barButtonSystemItem: .Refresh, target: self, action: "refresh")
+        let logoutButton = UIBarButtonItem(title: "Logout", style: .Plain, target: self, action: "logout")
+        self.navigationItem.rightBarButtonItem = refreshButton
+        self.navigationItem.leftBarButtonItem = logoutButton
+        
+        self.title = "On The Map"
+        println("hello world")
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +28,13 @@ class TabBarViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func logout() {
+        println("logout")
     }
-    */
+    
+    func refresh() {
+        println("refresh")
+    }
+    
 
 }
