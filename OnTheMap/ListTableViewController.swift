52618be59ae241e867fd1f52eb870e0841a2abbd
Kelly Innes <kinnes@gmail.com>
2015-07-08 23:05:36 -0400

    set up tableview and cell and open link in safari

--- OnTheMap/ListTableViewController.swift
+++ OnTheMap/ListTableViewController.swift
@@ -8,12 +8,30 @@
 
 import UIKit
 
-class ListTableViewController: UIViewController {
+class ListTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    let names = ["Alice","Brad","Candace","David","Ethyl","Francis","Gigi","Herod","Ines","Jack"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("nameCell") as! UITableViewCell
+        var image: UIImage = UIImage(named: "pinicon")!
+        cell.imageView!.image = image
+        cell.textLabel!.text = names[indexPath.row]
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        UIApplication.sharedApplication().openURL(NSURL(string: "http://udacity.com")!)
     }
 
 }
