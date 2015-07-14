3f2f52c778f20a45decae68e883404f544968bc9
Kelly Innes <kinnes@gmail.com>
2015-07-13 22:45:37 -0400

    begin adding login functionality

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -10,4 +10,38 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameString: UITextField!
+    
+    @IBOutlet weak var passwordString: UITextField!
+    
+    @IBAction func loginButtonPressed(sender: UIButton) {
+        let u = usernameString.text
+        let p = passwordString.text
+        login(u, passwordString: p)
+    }
+    
+    func login(usernameString: String, passwordString: String) {
+        let request = NSMutableURLRequest(URL: NSURL(string: "http://www.udacity.com/api/session")!)
+        request.HTTPMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.HTTPBody = "{\"udacity\": {\"username\": \"\(usernameString)\", \"password\": \"\(passwordString)\"}}".dataUsingEncoding(NSUTF8StringEncoding)
+        
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+            if error != nil {
+                println(error)
+                return
+            }
+            let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5))
+            println(NSString(data: newData, encoding: NSUTF8StringEncoding))
+            //TODO:
+            // 1. parse JSON
+            // 2. check whether "registered" == true
+            // 3. if so, present "rootNavVC", passing it the sessionID and the Key
+            // 4. if not, UIActivityView error message explaining what went wrong
+            // 5. also: spinner to disable view while API call's being made
+        }
+        task.resume()
+    }
 }
